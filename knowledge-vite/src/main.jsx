0205da074378a3f0ca12cd41840f6750ff16bc4f
Sarthak Wadhawan <82511746+sarthak-wadhawan@users.noreply.github.com>
2025-08-15 19:34:31 -0500

    fix analytics, again

--- knowledge-vite/src/main.jsx
+++ knowledge-vite/src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { inject } from '@vercel/analytics'
 import App from './App.jsx'
-
+import { Analytics } from '@vercel/analytics/react'
 // Initialize analytics (runs once when app loads)
 inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 )
